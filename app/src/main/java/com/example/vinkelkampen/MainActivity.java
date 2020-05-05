641bed2b190a13e7960bdaee2919263aab02be68
Adam Ottvar <adam.ottvar@gmail.com>
2020-05-05 22:31:16 +0200

    Added first version of participants list and activity.

--- app/src/main/java/com/example/vinkelkampen/MainActivity.java
+++ app/src/main/java/com/example/vinkelkampen/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.vinkelkampen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_KP_PLAYING = "com.example.vinkelkampen.EXTRA_KP";
+    private static Map<String, Integer> mapOfParticipants = new HashMap<>();
+    private static List<String> KPStyret = Arrays.asList("Adam", "Dany", "Jocke", "Tobbe", "Victor");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void populateWithKP(){
+        for (int i = 0; i < KPStyret.size(); i++) {
+            mapOfParticipants.put(KPStyret.get(i), 0);
+        }
+    }
+
+    public static void clearParticipants(){
+        mapOfParticipants.clear();
+    }
+
+    public static ArrayList<String> getParticipantsAsArray() {
+        Set<String> keys = mapOfParticipants.keySet();
+        return new ArrayList<String>(keys);
+    }
+
+    /** Called when the user hits the "new round" button **/
+    public void newRound(View view) {
+        Intent intent = new Intent(this, ParticipantsActivity.class);
+        if (view.getId() == R.id.button_kp) {
+            intent.putExtra(EXTRA_KP_PLAYING, true);
+        }
+        else {
+            intent.putExtra(EXTRA_KP_PLAYING, false);
+        }
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/vinkelkampen/ParticipantsActivity.java
@@ -0,0 +1,53 @@
+package com.example.vinkelkampen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ParticipantsActivity extends AppCompatActivity implements RecyclerAdapterParticipants.ItemClickListener {
+    RecyclerAdapterParticipants adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participants);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        boolean isKP = intent.getBooleanExtra(MainActivity.EXTRA_KP_PLAYING, false);
+
+        if (isKP) {
+            MainActivity.populateWithKP();
+        }
+        else {
+            MainActivity.clearParticipants();
+        }
+
+        // data to populate the RecyclerView with
+        ArrayList<String> participantsNames = MainActivity.getParticipantsAsArray();
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerParticipants);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerAdapterParticipants(this, participantsNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+}
